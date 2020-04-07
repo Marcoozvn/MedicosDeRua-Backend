ad4cd2572745033a44bef86752e5c79dd6b7561c
Marcos <marcos.cesar@ccc.ufcg.edu.br>
2020-04-07 10:10:54 -0300

    Configurações de produção

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const config = process.env.NODE_ENV == 'dev' ? process.env.DB_DEV_HOST : process.env.DB_TEST_HOST; 
 
-mongoose.connect(config, {
+mongoose.connect(process.env.DB_PROD || config, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
